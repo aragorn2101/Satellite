7f388665b0a49b2b26b4c27d1f2909904f56e4b8
Nitish Ragoomundun <lrugratz@gmail.com>
2024-03-17 20:50:59 +0400

    ESA_Copernicus/OData_download_v3.0.py: loop over records and update log file.

--- ESA_Copernicus/OData_download_v3.0.py
+++ ESA_Copernicus/OData_download_v3.0.py
@@ -45,10 +45,16 @@
 
 from sys import argv
 from os.path import isfile
+import json
+import requests
 import pandas as pd
 
 
-###  BEGIN Parsing of command line arguments  ###
+#  File containing token as a JSON record
+TokenFile = "CopernicusDataspace_token.json"
+
+
+###  BEGIN Parsing of command line arguments and load Token file  ###
 try:
     # Check if there is an argument
     if len(argv) <= 1:
@@ -62,9 +68,9 @@ try:
 
 except OSError:
     print("Usage: {:s} ODATA_QUERY_LOG".format(argv[0]))
-    print("The file ODATA_QUERY_LOG is a log file output by a OData_query.py script. The")
-    print("basic format of this input file is CSV with a few lines for preamble where the")
-    print("database query parameters are specified.")
+    print("The file ODATA_QUERY_LOG is a log file output by the OData_query.py script.")
+    print("The basic format of this input file is CSV with a few lines for preamble")
+    print("where the database query parameters are specified.")
     print()
     exit(1)
 except FileNotFoundError:
@@ -72,7 +78,16 @@ except FileNotFoundError:
     print()
     exit(2)
 
-###  END Parsing of command line arguments  ###
+# Token file
+try:
+    with open(TokenFile) as f:
+        tkn_dict = json.load(f)
+
+except FileNotFoundError:
+    print("Cannot access token file '{:s}'".format(TokenFile))
+    exit(3)
+
+###  END Parsing of command line arguments and load Token file  ###
 
 
 ###  BEGIN Open log file, parse header and load records in dataframe  ###
@@ -91,6 +106,66 @@ with open(LogFile) as f:
 log_df = pd.read_csv(LogFile, skiprows=count_hdr)
 
 ###  END Open log file, parse header and load records in dataframe  ###
+
+
+#  Template for writing preamble into log CSV file
+log_template = """\
+{0}---------------------
+{1}"""
+
+
+###  BEGIN LOOP through records and download  ###
+RecordIdx = 0
+while (RecordIdx < log_df.shape[0]):
+    # Check if file has already been downloaded, according to the log
+    if (log_df.loc[RecordIdx, 'Downloaded'] == True):
+        RecordIdx += 1
+
+    else:
+        # File name for data
+        OutFile = log_df.loc[RecordIdx, 'Name'] + ".zip"
+
+        print("\n------------------------------------------------------------------------------")
+        print("#  Working on record with index {:3d}".format(RecordIdx))
+        print("#  {:s}".format(OutFile))
+
+        # Download if data is found online and hasn't been downloaded yet
+        if (log_df.loc[RecordIdx, 'Online'] == True and log_df.loc[RecordIdx, 'Downloaded'] == False):
+
+            # Build URL for data product
+            url_data = "https://zipper.dataspace.copernicus.eu/odata/v1/Products({:s})/$value".format( log_df.loc[RecordIdx, 'Id'] )
+
+            # Build header using token
+            hdrs = { "Authorization" : "Bearer {:s}".format(tkn_dict['access_token']) }
+
+            # Open session and request data download
+            session = requests.Session()
+            session.headers.update( hdrs )
+            session_res = session.get(url_data, headers=hdrs, stream=True)
+
+            if (session_res.status_code == 200):  # everything ok
+
+                print("\nDownloading ...")
+
+                # Update log dataframe
+                log_df.loc[RecordIdx, 'Downloaded'] = True
+                RecordIdx += 1
+
+            else:
+                print("\nSession response status_code: {:d}".format(session_res.status_code))
+                print("Session response reason: {:s}".format(session_res.reason))
+                RecordIdx += 1
+
+        else:  # log_df['Online'] = False
+            print("Data not found online!")
+            RecordIdx += 1
+
+###  END LOOP through records and download  ###
+
+print("\n------------------------------------------------------------------------------")
+print("# Updating log file {:s} ...".format(LogFile))
+with open(LogFile, 'w') as f:
+    f.write(log_template.format(log_hdr, log_df.to_csv(index=False)))
 
 
 print()
